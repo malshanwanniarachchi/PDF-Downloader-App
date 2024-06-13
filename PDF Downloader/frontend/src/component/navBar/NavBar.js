f18c19e71445e4adad1e4e9fbb87cc361607fd73
Malshan Wanniarachchi <146846742+malshanwanniarachchi@users.noreply.github.com>
2024-06-13 22:12:36 +0530

    nav bar

--- PDF Downloader/frontend/src/component/navBar/NavBar.js
+++ PDF Downloader/frontend/src/component/navBar/NavBar.js
@@ -1,24 +1,22 @@
-import React from 'react'
+import React from 'react';
 
 export default function NavBar() {
   return (
-    <div>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="/home">Home</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="/add">Upload</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/list">List</a>
-      </li>
-    </ul>
-  </div>
-</nav>
-    </div>
-  )
+    <nav className="navbar navbar-expand-lg navbar-dark" style={{ backgroundColor: 'grey' }}>
+      <a className="navbar-brand" href="/home">  Home</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <a className="nav-link" href="/add">Upload</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/list">List</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
